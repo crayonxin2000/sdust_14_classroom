6f3db954f8d67e15eef20c156e72624f78dd5261
crayonxin2000 <zxh20000908@163.com>
2021-11-27 12:52:58 +0800

    更新11.27

--- outExcels.py
+++ outExcels.py
@@ -14,7 +14,7 @@ def getCookieDict(cook):
     return cookies
 
 url="http://jwgl.sdust.edu.cn/jsxsd/kbcx/kbxx_classroom_ifr"
-cookie_text="UM_distinctid=178dabf846387e-0335b78fc680db-3f356b-144000-178dabf8464b70; JSESSIONID=70B9FB07EFB9710158B83A14CFAF1590"
+cookie_text="JSESSIONID=ADE58AEA0F0353A95F3B708DF5E195A0"
 
 
 def html_to_excel(d,index):
