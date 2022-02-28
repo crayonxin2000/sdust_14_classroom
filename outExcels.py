2a09f9baa8e7943bcb8e74f1f3e40c68bd46c631
crayonxin2000 <zxh20000908@163.com>
2022-02-28 15:46:54 +0800

    2021-2022-2学期课表第一次更新

--- outExcels.py
+++ outExcels.py
@@ -1,3 +1,12 @@
+'''
+Author: crayonxin
+Date: 2021-08-28 20:42:18
+LastEditTime: 2022-02-28 15:40:59
+LastEditors: crayonxin
+Description: 
+FilePath: \sdust_14_classroom\outExcels.py
+
+'''
 
 import bs4
 from pandas.io import html
@@ -14,7 +23,7 @@ def getCookieDict(cook):
     return cookies
 
 url="http://jwgl.sdust.edu.cn/jsxsd/kbcx/kbxx_classroom_ifr"
-cookie_text="JSESSIONID=ADE58AEA0F0353A95F3B708DF5E195A0"
+cookie_text="yikikata=1b3f1381-54df957b501e945f2a8b6eae619cb0b6; brmidyrvj=both; UM_distinctid=17e347770289ca-0fa89abc4d763d-4303066-144000-17e34777029abe; JSESSIONID=705D616322A32CB30FFAEA7804B03D21; yikikata=1b3f13d0-319c6a6c301f343c01eb03551046e48b; brmidyrvj=both"
 
 
 def html_to_excel(d,index):
@@ -26,7 +35,7 @@ def html_to_excel(d,index):
 
 
 data={}
-data["xnxqh"]="2021-2022-1"
+data["xnxqh"]="2021-2022-2"
 data["xqid"]=1
 data["jzwid"]=14
 for i in range(18):# 本学期有18周
